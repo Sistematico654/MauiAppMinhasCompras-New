df6f0861b3a1c96a11b0005663c249a795338487
João <jhsp350@gmail.com>
2025-09-13 13:51:40 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MauiAppMinhasCompras.Helpers;
+using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("pt-BR");
+
+
+
+namespace MauiAppMinhasCompras
+{
+    public partial class App : Application
+    {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if(_db == null) 
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            // MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
+    }
+}
---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,54 @@
+﻿using SQLite;
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produto
+    {
+        string _descricao;
+        double _quantidade;
+        double _preco;
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a Descrição");
+                }
+
+                _descricao = value;
+            }
+        }
+        public double Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por favor, preencha a Quantidade");
+                }
+
+                _quantidade = value;
+            }
+        }
+        public double Preco
+        {
+            get => _preco;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por favor, preencha o Preço");
+                }
+
+                _preco = value;
+            }
+        }
+
+        public double Total { get => Quantidade * Preco; }
+    }
+};
